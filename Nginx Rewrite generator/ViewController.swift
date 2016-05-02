cf78a9b7952b0343a8270fec6b1e02b192289aac
arioch1984 <arioch1984@gmail.com>
2016-05-02 14:52:37 +0200

    first

--- Nginx Rewrite generator/ViewController.swift
+++ Nginx Rewrite generator/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("test");
     }
 
     override var representedObject: AnyObject? {
